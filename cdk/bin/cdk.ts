fedea9fb8a2161d80898a853785b1d25b0ad6e5f
junoha <jun.ohashi.42@gmail.com>
2021-11-26 22:23:22 +0900

    Add SNS step

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -6,33 +6,40 @@ import { Tags } from '@aws-cdk/core';
 
 const app = new cdk.App();
 
-const s3BucketName: string = app.node.tryGetContext('s3BucketName');
+const s3BucketName: string = app.node.tryGetContext('s3BucketName') ?? process.env.S3_BUCKET_NAME;;
 if (!s3BucketName) {
-  console.warn('Invalid s3BucketName. s3BucketName must be set');
+  console.error('Invalid s3BucketName. s3BucketName must be set');
   process.exit(1);
 }
 
-const s3Prefix: string = app.node.tryGetContext('s3Prefix');
+const s3Prefix: string = app.node.tryGetContext('s3Prefix') ?? process.env.S3_PREFIX;;
 if (!s3Prefix) {
-  console.warn('Invalid s3Prefix. s3Prefix must be set');
+  console.error('Invalid s3Prefix. s3Prefix must be set');
   process.exit(1);
 }
 
-const semaphore: string = app.node.tryGetContext('semaphore');
+const semaphore: string = app.node.tryGetContext('semaphore') ?? process.env.SEMAPHORE;;
 if (!semaphore) {
-  console.warn('Invalid semaphore. semaphore must be set');
+  console.error('Invalid semaphore. semaphore must be set');
   process.exit(1);
 }
 if (isNaN(parseInt(semaphore))) {
-  console.warn('Invalid semaphore. semaphore must be number');
+  console.error('Invalid semaphore. semaphore must be number');
   process.exit(1);
 }
 const semaphoreInt = parseInt(semaphore);
 
+const snsTopicArn: string = app.node.tryGetContext('snsTopicArn') ?? process.env.SNS_TOPIC_ARN;
+if (!snsTopicArn) {
+  console.error('snsTopicArn must be set. -c snsTopicArn=yyy or SNS_TOPIC_ARN=yyy');
+  process.exit(1);
+}
+
 const props = {
   s3BucketName,
   s3Prefix,
-  semaphore: semaphoreInt
+  semaphore: semaphoreInt,
+  snsTopicArn,
 };
 
 const stack = new AwsDocSearchStack(app, 'AwsDocSearchStack', props);
